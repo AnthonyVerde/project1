2eb1776bbfd42649c059fee4e7db6a1d6cec67f4
Alex Yepes <alexisyepes@yahoo.com>
2019-01-10 00:31:23 -0500

    first commit with working APIs, still working on resize results

---
+++ flagsAPI/project1.js
@@ -0,0 +1,34 @@
+$("#find-flag").on("click", function(event) {
+
+ 
+  event.preventDefault(); 
+
+  var country = $("#country-input").val();
+
+  var queryURL = "https://restcountries.eu/rest/v2/name/" + country;
+
+$("#flags").empty();
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+
+
+for (i=0; i < response.length; i++) {
+  var result = response[i].flag;
+}
+
+console.log(i);
+
+  var flagImage = $("<img>").attr("src", result);
+  $("#flags").append(flagImage);
+
+  
+
+
+
+
+});
+});
+
