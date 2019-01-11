36485bd2700b2410e94a7d17855d019b6fcdde5a
juanidiaz <jdi@idiaz.ca>
2019-01-10 22:35:16 -0500

    Working draft

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,29 +1,32 @@
 // VARIABLES --------------------------------------------
 
 //      OBJECTS
-
+var intervalId; // Holds setInterval that runs the stopwatch
 
 
 //      ARRAYS
-var cardsArray = [];
-var urlArray = [];
-var indexArray = [];
+var cardsArray = []; // Will hold the URLs fr all cards in te grid
+var urlArray = []; // Will hold the URL of card one and card two
+var indexArray = []; // Will hold the index of card one and card two
 
 //      STRINGS/CHAR
-var username = '';
-var mode = '';
+var userName = ''; // User's name
+var userCountry = ''; // User's country name
+var mode = ''; // Type of game selected [EASY, TIMED, CHALLENGE]
+var firstPick = ""; // Index of the first card picked
+var secondPick = ""; // Index of the second card picked
 
 //      NUMBER/INTEGER
-var pairs = 0;
-var tries = 0;
-var time = 0;
-var games = 0;
-var pairsMatched = 0;
+var pairs = 0; // Pair of cards that will be in the grid
+var tries = 0; // Number of pairs fliped in a game
+var time = 0; // Time to beat every game
+var games = 0; // Number of games played
+var pairsMatched = 0; // Number of pairs matched in a game
 
 //      BOOLEAN
-var timer = false;
-var challenge = false;
-var firstPick = false;
+var timer = false; // The game requires to show and use timer
+var challenge = false; // The player is on 'challenge' mode
+var finishGame = false; // TRUE if all pairs have been found in a game
 
 
 // Creating a "player info" object using constructor notation
@@ -34,10 +37,11 @@ function playerInfo(playerName, playerCountry, ) {
 
 var player = new playerInfo('', ''); // Contains CURRENT player info
 
-
 // ------------------------------------------------------------
 
 $(document).ready(function () {
+
+    // $("#gifBox").css("pointer-events","none");
 
     /*******************************************
      * Randomize array element order in-place. *
@@ -59,7 +63,7 @@ $(document).ready(function () {
 
         //////////////////////// GIPHY ////////////////////////
         $.ajax({
-            url: "https://api.giphy.com/v1/gifs/search?api_key=lKEjHvIVGBtk7Z1Ai1vo4y0bqkX3CHJp&q=numbers&limit=8",
+            url: "https://api.giphy.com/v1/gifs/search?api_key=lKEjHvIVGBtk7Z1Ai1vo4y0bqkX3CHJp&q=colors&limit=" + pairs,
             method: "GET"
         }).then(function (res) {
 
@@ -77,13 +81,36 @@ $(document).ready(function () {
         });
         //////////////////////// GIPHY ////////////////////////
 
+        console.log("calling the clock")
+        if (mode === "timed" || mode === "challenge") {
+            timerRun(60);
+        } else {
+            $("#clock").hide();
+        }
+
     }
 
     // Show the cards in the board
     function displayCards() {
 
         // Resize the container of the cards
-        $("#gifBox").css("width", 500);
+        switch (parseInt(pairs)) {
+            case 2:
+                var boxWidth = 240;
+                break;
+
+            case 3, 4:
+                var boxWidth = 360;
+                break;
+
+            default:
+                var boxWidth = 480;
+                break;
+        }
+
+        console.log("Pairs: " + pairs + "   Width: " + boxWidth);
+
+        $("#gifBox").css("width", boxWidth);
 
         // Remove all the existing cards
         $("#gifs").html("");
@@ -92,7 +119,7 @@ $(document).ready(function () {
         for (var c in cardsArray) {
 
             var cardDiv = $("<div>").addClass("card m-2");
-            var cardImg = $("<img>").attr("src", cardsArray[c]).attr("id", "front" + c).css("display", "none").addClass("staticgif card-img-top").appendTo(cardDiv);
+            var cardImg = $("<img>").attr("src", cardsArray[c]).attr("id", "frnt" + c).css("display", "none").addClass("staticgif card-img-top").appendTo(cardDiv);
             var cardback = $("<img>").attr("src", "https://placehold.it/100x100").attr("id", "back" + c).attr("data-url", cardsArray[c]).addClass("staticgif card-img-top").appendTo(cardDiv);
 
             // Append each card
@@ -100,9 +127,8 @@ $(document).ready(function () {
         }
     }
 
-    // Update player stats
+    // Update the screen
     function updateScreen() {
-
 
         switch (mode) {
             case 'easy':
@@ -110,6 +136,7 @@ $(document).ready(function () {
                 $("#game").show();
                 $("#stats").show();
 
+                updateStats();
                 break;
 
             case 'timed':
@@ -117,6 +144,7 @@ $(document).ready(function () {
                 $("#game").show();
                 $("#stats").show();
 
+                updateStats();
                 break;
 
             case 'challenge':
@@ -124,6 +152,7 @@ $(document).ready(function () {
                 $("#game").show();
                 $("#stats").show();
 
+                updateStats();
                 break;
 
             default:
@@ -132,23 +161,26 @@ $(document).ready(function () {
                 $("#stats").hide();
                 break;
         }
-
-
-
-    }
+    };
 
     // Update player stats
     function updateStats() {
 
         $("#info").html("");
-        var gameStats = $("<h2>").text("Pairs matched: " + pairsMatched + "  Tries: " + tries).appendTo($("#info"));
+
+        if (!finishGame) {
+            var gameStats = $("<h2>").text("Pairs matched: " + pairsMatched + "  Tries: " + tries).appendTo($("#info"));
+        } else {
+            var gameStats = $("<h1>").text("FOUND ALL PAIRS!").appendTo($("#info"));
+        }
+
+
     }
 
     // Click on back of card
     $("#gifs").on("click", ".staticgif", function () {
 
-        // If the same card is clicked twice do nothing
-        if (this.id.substr(4) === urlArray[0]) {
+        if (secondPick != "") {
             return;
         }
 
@@ -156,11 +188,24 @@ $(document).ready(function () {
         // Making sure to remove the first 4 characters
         var choice = this.id.substr(4);
 
+        // If the same card is clicked twice do nothing
+        if (this.id.substr(4) === firstPick) {
+            console.log("repeated");
+            return;
+        }
+
+        if (firstPick === "") {
+            firstPick = choice;
+        } else if (firstPick != "" && secondPick === "") {
+
+            secondPick = choice;
+        }
+
         // Hide the back of the card
         $("#back" + choice).hide();
 
         // Show the front of the card
-        $("#front" + choice).show();
+        $("#frnt" + choice).show();
 
         // Get the URL for the card
         urlArray.push(this.dataset.url)
@@ -168,15 +213,7 @@ $(document).ready(function () {
         // Get the INDEX for the card
         indexArray.push(choice);
 
-        if (!firstPick) {
-
-            // Marked that the first card was picked
-            firstPick = true;
-        } else {
-
-            // Switch back that the first card was picked
-            firstPick = false;
-
+        if (firstPick != "" && secondPick != "") {
             // Add a try to the list
             tries++;
 
@@ -185,34 +222,45 @@ $(document).ready(function () {
                 if (urlArray[0] === urlArray[1]) {
                     // The cards match, hide both cards
 
-                    console.log("CARDS MATCH!!");
+                    // console.log("CARDS MATCH!!");
 
                     // Hiding the front of the card
-                    $("#front" + indexArray[0]).css("visibility", "hidden");
-                    $("#front" + indexArray[1]).css("visibility", "hidden");
+                    $("#frnt" + indexArray[0]).css("visibility", "hidden");
+                    $("#frnt" + indexArray[1]).css("visibility", "hidden");
 
                     // Add a pair matched to the list
                     pairsMatched++;
 
+                    if (pairsMatched * 2 === cardsArray.length) {
+
+                        console.log("FINISHED ALL CARDS!");
+                        finishGame = true;
+                    }
+
                 } else {
                     // The cards dont match, flip tham back
-                    console.log("Not a match");
+                    // console.log("Not a match");
 
                     // Hide the back of the card
                     $("#back" + indexArray[0]).show();
-                    $("#front" + indexArray[0]).hide();
+                    $("#frnt" + indexArray[0]).hide();
                     $("#back" + indexArray[1]).show();
-                    $("#front" + indexArray[1]).hide();
+                    $("#frnt" + indexArray[1]).hide();
                 }
 
                 // Empty the URL and index array
                 urlArray = [];
                 indexArray = [];
 
+                // Switch back that the first card was picked
+                firstPick = "";
+                secondPick = "";
+
+                // Update the game stats
+                updateStats();
+
             }, 2000); // Wait this many miliseconds after the second card is picked
 
-            // Update the game stats
-            updateStats();
 
         }
 
@@ -256,6 +304,48 @@ $(document).ready(function () {
 
     })
 
+
+    /********** ALL TIMER RELATED FUNCTIONS **********/
+    function timerRun(timeToBeat) {
+
+        // Stop timer
+        timerStop();
+
+        // Set time for the game
+        time = timeToBeat;
+
+        // Set interval to 1 second
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+    }
+
+    function timerStop() {
+        // Return clock to 00
+        clearInterval(intervalId);
+    }
+
+    function decrement() {
+
+        //  Decrease time by one.
+        time--;
+
+        //  Update the time 
+        $("#clock").text(time);
+
+        //  When run out of time...
+        if (time <= 0) {
+
+            //  Update the time 
+            $("#clock").text("Time's up!");
+
+            // Stop timer
+            timerStop();
+
+            // Log "out of time" and question number
+            console.log("Clock down");
+        }
+    }
+    /*************************************************/
 
 
 
