9c9fae10d107c4a3dfa9bb9441503da7bc6c2d82
juanidiaz <jdi@idiaz.ca>
2019-01-12 14:43:11 -0500

    Updated
    - Implemented API for character images
    - Modified card size to match new images
    - Added design for card back
    - Added prety time
    - Arrange elements on game mode
    - Stats now on the left
    - Added buttons

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -61,32 +61,55 @@ $(document).ready(function () {
         // Empty array with URL of cards
         cardsArray = [];
 
-        //////////////////////// GIPHY ////////////////////////
+        //////////////////////// superheroapi ////////////////////////
+        const KEY = '10161297457820113';
+
+        //var queryURL = `https://cors-anywhere.herokuapp.com/http://superheroapi.com/api.php/${KEY}/search/man`;
+        var queryURL = `http://superheroapi.com/api.php/${KEY}/search/man`;
+
+
         $.ajax({
-            url: "https://api.giphy.com/v1/gifs/search?api_key=lKEjHvIVGBtk7Z1Ai1vo4y0bqkX3CHJp&q=colors&limit=" + pairs,
+            url: queryURL,
             method: "GET"
-        }).then(function (res) {
+        }).then(function (response) {
 
             // Get the URL to the image into the cardsArray... twice!
-            for (var g in res.data) {
-                cardsArray.push(res.data[g].images.fixed_height_still.url);
-                cardsArray.push(res.data[g].images.fixed_height_still.url);
+            // note to [22,23,50,57] Numbers to avoid(no Imgs)????
+
+            // Built carsArray with URL to character images
+            for (let p = 0; p < pairs; p++) {
+
+                // Generate random number between 1 and 63 - to randomize character image
+                var index = Math.floor(Math.random() * 63) + 1;
+
+                // Confirm the index is not one of th eones with missing images
+                switch (index) {
+                    case 22:
+                    case 23:
+                    case 50:
+                    case 57:
+                        // Yup... no images for these indexes... try again
+                        //console.log("Selected: " + index + "  Trying again");
+
+                        // Reduce the iteration variable so to get a new value
+                        p--;
+                        break;
+
+                    default:
+                        // For all other cases push the URL to the image into cardsArray... twice! 
+                        cardsArray.push(response.results[index].image.url);
+                        cardsArray.push(response.results[index].image.url);
+                        break;
+                }
             }
 
-            // Ranomize the positions of the cardsArray
+            // Randomize the positions of the cardsArray
             shuffleArray(cardsArray);
 
             // Show the BACK of the cards
             displayCards();
         });
-        //////////////////////// GIPHY ////////////////////////
-
-        console.log("calling the clock")
-        if (mode === "timed" || mode === "challenge") {
-            timerRun(60);
-        } else {
-            $("#clock").hide();
-        }
+        //////////////////////////////////////////////////////////////
 
     }
 
@@ -96,20 +119,24 @@ $(document).ready(function () {
         // Resize the container of the cards
         switch (parseInt(pairs)) {
             case 2:
-                var boxWidth = 240;
+                // For 2 pairs only (4 cards)
+                var boxWidth = 250;
                 break;
 
             case 3, 4:
-                var boxWidth = 360;
+                // For 3 pairs (6 cards) and 4 pairs (8 cards)
+                var boxWidth = 370;
                 break;
 
             default:
-                var boxWidth = 480;
+                // For 5 pairs or more
+                var boxWidth = 490;
                 break;
         }
 
-        console.log("Pairs: " + pairs + "   Width: " + boxWidth);
+        //console.log("Pairs: " + pairs + "   Width: " + boxWidth);
 
+        // Set the with of the cards grid
         $("#gifBox").css("width", boxWidth);
 
         // Remove all the existing cards
@@ -118,9 +145,14 @@ $(document).ready(function () {
         // For each GIF append an element to the DOM
         for (var c in cardsArray) {
 
+            // Create a CARD div
             var cardDiv = $("<div>").addClass("card m-2");
+
+            // Append the character image to the front of the card, but HIDE this image - this to mimic the fact of the card being "facing donw"
             var cardImg = $("<img>").attr("src", cardsArray[c]).attr("id", "frnt" + c).css("display", "none").addClass("staticgif card-img-top").appendTo(cardDiv);
-            var cardback = $("<img>").attr("src", "https://placehold.it/100x100").attr("id", "back" + c).attr("data-url", cardsArray[c]).addClass("staticgif card-img-top").appendTo(cardDiv);
+
+            // Append the image of the back if the card - this to mimic the fact of the card being "facing donw"
+            var cardback = $("<img>").attr("src", "./assets/images/card_back.png").attr("id", "back" + c).attr("data-url", cardsArray[c]).addClass("staticgif card-img-top").appendTo(cardDiv);
 
             // Append each card
             $("#gifs").append(cardDiv);
@@ -130,35 +162,56 @@ $(document).ready(function () {
     // Update the screen
     function updateScreen() {
 
+        console.log("UPDATING");
+
+        console.log("calling the clock")
+        if (mode === "timed" || mode === "challenge") {
+            timerRun(128);
+        } else {}
+
+
         switch (mode) {
             case 'easy':
+                console.log("EASY MODE");
                 $("#welcome").hide();
                 $("#game").show();
-                $("#stats").show();
+
+                $("#box-clock").hide();
+                $("#box-matches").hide();
+                $("#box-level").hide();
 
                 updateStats();
                 break;
 
             case 'timed':
+                console.log("TIMED MODE");
                 $("#welcome").hide();
                 $("#game").show();
-                $("#stats").show();
+
+                $("#box-clock").show();
+                $("#box-matches").hide();
+                $("#box-level").hide();
 
                 updateStats();
                 break;
 
             case 'challenge':
+                console.log("CHALLENGE MODE");
                 $("#welcome").hide();
                 $("#game").show();
-                $("#stats").show();
+
+                $("#box-clock").show();
+                $("#box-matches").show();
+                $("#box-level").show();
 
                 updateStats();
                 break;
 
             default:
+                console.log("DEFAULT MODE");
                 $("#welcome").show();
                 $("#game").hide();
-                $("#stats").hide();
+                // $("#stats").hide();
                 break;
         }
     };
@@ -166,10 +219,18 @@ $(document).ready(function () {
     // Update player stats
     function updateStats() {
 
-        $("#info").html("");
+        $("#app-logo").css("text-align", "start");
+        $("#app-logo-img").css("width", "290px");
+        $("#app-title").hide();
+
+
+        $("#mode_lbl").text(mode.toLocaleUpperCase() + "MODE");
 
         if (!finishGame) {
-            var gameStats = $("<h2>").text("Pairs matched: " + pairsMatched + "  Tries: " + tries).appendTo($("#info"));
+            $("#matches").text(matches);
+            $("#pairsm").text(pairsMatched);
+            $("#tries").text(tries);
+            $("#level").text(level);
         } else {
             var gameStats = $("<h1>").text("FOUND ALL PAIRS!").appendTo($("#info"));
         }
@@ -330,7 +391,7 @@ $(document).ready(function () {
         time--;
 
         //  Update the time 
-        $("#clock").text(time);
+        $("#clock").text(fancyTimeFormat(time));
 
         //  When run out of time...
         if (time <= 0) {
@@ -345,11 +406,31 @@ $(document).ready(function () {
             console.log("Clock down");
         }
     }
+
+    function fancyTimeFormat(time) {
+        // Hours, minutes and seconds
+        // ~~ is a shorthand for Math.floor
+
+        var hrs = ~~(time / 3600);
+        var mins = ~~((time % 3600) / 60);
+        var secs = ~~time % 60;
+
+        // Output like "1:01" or "4:03:59" or "123:03:59"
+        var ret = "";
+
+        if (hrs > 0) {
+            ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+        }
+
+        ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+        ret += "" + secs;
+        return ret;
+    }
+
     /*************************************************/
 
-
-
-
+    // Update screen
+    updateScreen();
 
 
 });
